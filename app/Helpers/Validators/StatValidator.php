8dc7374e08f1c142a0f45bf5cb8098331278311e
Istiaq Hasan <istiaq.me@gmail.com>
2022-02-11 17:48:33 +0600

    API Routes got grouped by the Middleware

--- app/Helpers/Validators/StatValidator.php
+++ app/Helpers/Validators/StatValidator.php
@@ -9,39 +9,7 @@ use App\Helpers\TextualData;
 
 class StatValidator
 {
-    public $request;
-
-    public function __construct(Request $request){
-        $this->request = $request;
-        $this->validateUserData();
-    }
-    /* 
-        @checks provided user data is validated according to the system
-        @params: object
-        @return: array
-    */
-    public function validateUserData ()
-    {
-
-        // key exists - email
-        if(!$this->request->has('email')){
-            return response()->json([
-                'type' => 'Error',
-                'msg' => "Email Not Found",
-                'data' => null
-            ], 200);
-        }
-        // key exists - password
-        if(!$this->request->has('password')){
-            return response()->json([
-                'type' => 'Error',
-                'msg' => "Password Not Found",
-                'data' => null
-            ], 200);
-        }
-
-        return $this;
-    }
+    /* deprecated */
 
 
 }
